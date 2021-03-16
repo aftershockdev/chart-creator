8b381c2ae2917ef21ec551b65b18e36ce5660fed
Ruslan Zherebytskyi <ruslanzh@sweetsoft.com.ua>
2021-03-16 19:01:57 +0200

    fixes

--- app.ts
+++ app.ts
@@ -3,14 +3,13 @@ let testData: Object[] = [];
 async function fetchData() {
   try {
     const response = await fetch(
-      "https://api.covid19api.com/live/country/ukraine/status/confirmed"
+      "https://api.covid19api.com/total/country/south-africa/status/confirmed?from=2020-03-21T00:00:00Z&to=2021-03-01T00:00:00Z"
     );
     const data = await response.json();
-    // let slicedData = data.length ? data.slice(0, 1000) : [];
     new ChartsCreator(".main", { data: data, type: "Bar" }).init();
     return data;
   } catch (error) {
-    throw new Error(error);
+    console.log(error);
   }
 }
 fetchData();
@@ -22,6 +21,10 @@ interface ObjectConstructor {
 interface line {
   date: number;
   value: number;
+}
+interface bar {
+  date: number;
+  value: string;
 }
 
 interface SettingsInterface extends ChartsInterface {
@@ -71,7 +74,7 @@ class ChartsCreator implements ChartsInterface {
   lineCurve: d3.CurveFactoryLineOnly;
 
   defaults = {
-    width: 900,
+    width: 1300,
     height: 400,
     margin: {
       top: 15,
@@ -93,37 +96,38 @@ class ChartsCreator implements ChartsInterface {
     this.dataConfigurator();
   }
 
-  private dataConfigurator(): void {
-    const configData = this.data.map((el: any) => {
-      const format = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
-      const obj = {
-        date: format(el.Date),
-        value: el.Confirmed,
-      };
-      return obj;
-    });
-    this.data = configData;
-    console.log(this);
-  }
-
   public init(): void {
     const { margin } = this;
-
     const svg = (this.svg = d3
       .select(this.element)
       .append("svg")
       .attr("width", this.width)
       .attr("height", this.height)
-      .attr("overflow", "visible")
-      .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`));
+      .attr("overflow", "visible"));
+    this.createBarChart(this.data);
+    // this.createLineChart(this.data);
+  }
 
+  private dataConfigurator(): void {
+    const configData = this.data.map((el: any) => {
+      // const format = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
+      const obj = {
+        date: el.Date,
+        value: el.Cases,
+      };
+      return obj;
+    });
+    configData.sort((a, b) => b.value - a.value);
+    this.data = configData;
+  }
+
+  private createLineChart(data: object[] | any): void {
     let x = d3
       .scaleTime()
       .domain(d3.extent(this.data, (d: line) => d.date))
       .range([0, this.width]);
 
-    let xAxis = svg
+    let xAxis = this.svg
       .append("g")
       .attr("transform", "translate(0," + this.height + ")")
       .call(d3.axisBottom(x));
@@ -133,22 +137,13 @@ class ChartsCreator implements ChartsInterface {
       .domain([0, d3.max(this.data, (d: line) => +d.value)])
       .range([this.height, 0]);
 
-    let yAxis = svg.append("g").call(d3.axisLeft(y));
+    let yAxis = this.svg.append("g").call(d3.axisLeft(y));
 
-    this.renderLineChart(x, y);
-  }
-
-  private renderLineChart(
-    x:
-      | d3.ScaleTime<number, number, never>
-      | d3.ScaleLinear<number, number, never>,
-    y:
-      | d3.ScaleTime<number, number, never>
-      | d3.ScaleLinear<number, number, never>
-  ): void {
     const scaleX = (this.scaleY = d3.scaleTime().range([0, innerWidth]));
 
     const scaleY = (this.scaleY = d3.scaleLinear().range([innerHeight, 0]));
+
+    const dateFormatter = d3.timeFormat("%Y/%m/%d");
 
     this.line = d3
       .line<line>()
@@ -193,9 +188,9 @@ class ChartsCreator implements ChartsInterface {
 
     area
       .append("path")
-      .datum(this.data)
+      .datum(data)
       .attr("class", "myArea")
-      .attr("fill", "red")
+      .attr("fill", "lightgrey")
       .attr("fill-opacity", 0.3)
       .attr("stroke", "grey")
       .attr("stroke-width", 1)
@@ -228,26 +223,28 @@ class ChartsCreator implements ChartsInterface {
       .attr("x", 30)
       .attr("y", 0);
 
-    function mousemove() {
-      let x0: any = x.invert(d3.pointer("mousemove", this)[0]),
-        i = bisectDate(this.data, x0, 1),
-        d0 = this.data[i - 1],
-        d1 = this.data[i],
+    function mousemove(): void {
+      let x0: any = x.invert(d3.pointer(event, this)[0]),
+        i = bisectDate(data, x0, 1),
+        d0 = data[i - 1],
+        d1 = data[i],
         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
       focus.attr(
         "transform",
         "translate(" + x(d.date) + "," + y(d.value) + ")"
       );
-      focus.select(".tooltip-date").text(d.date);
-      focus.select(".tooltip-likes").text(d.value + "$");
+      focus.select(".tooltip-date").text(dateFormatter(d.date));
+      focus.select(".tooltip-likes").text(d.value);
     }
 
     function idled() {
       idleTimeout = null;
     }
 
-    function updateChart(): number {
-      let extent = d3.selection;
+    function updateChart({
+      selection,
+    }: d3.D3BrushEvent<typeof selection>): number {
+      let extent = selection;
       if (!extent) {
         if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350));
         x.domain([4, 8]);
@@ -255,7 +252,7 @@ class ChartsCreator implements ChartsInterface {
         x.domain([x.invert(extent[0]), x.invert(extent[1])]);
         area.select(".brush").call(brush.move, null);
       }
-      this.xAxis.transition().duration(1000).call(d3.axisBottom(x));
+      xAxis.transition().duration(1000).call(d3.axisBottom(x));
       area
         .select(".myArea")
         .transition()
@@ -264,21 +261,237 @@ class ChartsCreator implements ChartsInterface {
     }
 
     this.svg
-      // .on("dblclick", function (): any {
-      //   x.domain(d3.extent(this.data, (d: line) => d.date));
-      //   this.xAxis.transition().call(d3.axisBottom(x));
-      //   area.select(".myArea").transition().attr("d", areaGenerator);
-      // })
-      .on("mouseover", function () {
+      .on("dblclick", function (): void {
+        x.domain(d3.extent(data, (d: line) => d.date));
+        xAxis.transition().call(d3.axisBottom(x));
+        area.select(".myArea").transition().attr("d", areaGenerator);
+      })
+      .on("mouseover", function (): void {
         focus.style("display", null);
       })
-      .on("mouseout", function () {
+      .on("mouseout", function (): void {
         focus.style("display", "none");
       })
       .on("mousemove", mousemove);
   }
 
-  dimensions(): number[] {
+  private createBarChart(data: object[] | any): void {
+    let maxLength: any = d3.max(data.map((d) => d.date.length));
+    let marginOverview = { top: 30, right: 10, bottom: 20, left: 40 };
+    let barWidth = maxLength * 5;
+    let numBars = Math.round(this.width / barWidth);
+    let isScrollDisplayed = barWidth * data.length > this.width;
+    let heightOverview = 80 - marginOverview.top - marginOverview.bottom;
+    let selectorHeight = 40;
+
+    const dateFormatter = d3.timeFormat("%Y/%m/%d");
+
+    let xscale = d3
+      .scaleBand()
+      .domain(
+        data.slice(0, numBars).map(function (d) {
+          return d.date;
+        })
+      )
+      .range([0, this.width]);
+
+    let yscale = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(data.slice(0, numBars), function (d: bar) {
+          return +d.value;
+        }),
+      ])
+      .range([this.height, 0]);
+
+    let diagram = this.svg
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")"
+      );
+
+    diagram
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0, " + this.height + ")")
+      .call(d3.axisBottom(xscale));
+
+    diagram.append("g").attr("class", "y axis").call(d3.axisLeft(yscale));
+
+    let bars = diagram.append("g");
+
+    bars
+      .selectAll("rect")
+      .data(data.slice(0, numBars), function (d: bar) {
+        return d.date;
+      })
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("y", this.height)
+      .attr("height", 0)
+      .transition()
+      .duration(750)
+      .delay(function (d, i) {
+        return i * 150;
+      })
+      .attr("x", function (d: any) {
+        return xscale(d.date);
+      })
+      .attr("y", (d) => yscale(d.value))
+      .attr("height", (d: line) => this.height - yscale(d.value))
+      .attr("width", xscale.bandwidth() - 3);
+
+    // if (isScrollDisplayed) {
+    //   let xOverview = d3
+    //     .scaleBand()
+    //     .domain(
+    //       data.map(function (d) {
+    //         return d.label;
+    //       })
+    //     )
+    //     .range([0, this.width]);
+
+    //   let yOverview = d3.scaleLinear().range([heightOverview, 0]);
+    //   yOverview.domain(yscale.domain());
+
+    //   let subBars = diagram.selectAll(".subBar").data(data);
+
+    //   subBars
+    //     .enter()
+    //     .append("rect")
+    //     .classed("subBar", true)
+    //     .attr({
+    //       height: function (d) {
+    //         return heightOverview - yOverview(d.value);
+    //       },
+    //       width: function (d) {
+    //         return xOverview.rangeBand();
+    //       },
+    //       x: function (d) {
+    //         return xOverview(d.label);
+    //       },
+    //       y: function (d) {
+    //         return height + heightOverview + yOverview(d.value);
+    //       },
+    //     });
+
+    //   var displayed = d3
+    //     .scaleQuantize()
+    //     .domain([0, this.width])
+    //     .range(d3.range(data.length));
+
+    //   diagram
+    //     .append("rect")
+    //     .attr(
+    //       "transform",
+    //       "translate(0, " + (this.height + this.margin.bottom) + ")"
+    //     )
+    //     .attr("class", "mover")
+    //     .attr("x", 0)
+    //     .attr("y", 0)
+    //     .attr("height", selectorHeight)
+    //     .attr(
+    //       "width",
+    //       Math.round(parseFloat(numBars * this.width) / data.length)
+    //     )
+    //     .attr("pointer-events", "all")
+    //     .attr("cursor", "ew-resize")
+    //     .call(d3.drag().on("drag", display));
+
+    //   this.svg.on("scroll", display);
+    // }
+    // function display() {
+    //   let x = parseInt(d3.select(this).attr("x")),
+    //     nx = x + d3.event.dx,
+    //     w = parseInt(d3.select(this).attr("width")),
+    //     f,
+    //     nf,
+    //     new_data,
+    //     rects;
+
+    //   if (nx < 0 || nx + w > width) return;
+
+    //   d3.select(this).attr("x", nx);
+
+    //   f = displayed(x);
+    //   nf = displayed(nx);
+
+    //   if (f === nf) return;
+
+    //   new_data = data.slice(nf, nf + numBars);
+
+    //   xscale.domain(
+    //     new_data.map(function (d) {
+    //       return d.label;
+    //     })
+    //   );
+    //   diagram.select(".x.axis").call(xAxis);
+
+    //   rects = bars.selectAll("rect").data(new_data, function (d) {
+    //     return d.label;
+    //   });
+
+    //   label = labels.selectAll(".label").data(new_data, function (d) {
+    //     return d.label;
+    //   });
+
+    //   rects.attr("x", function (d) {
+    //     return xscale(d.label);
+    //   });
+    //   label.attr("x", function (d) {
+    //     return xscale(d.label);
+    //   });
+
+    //   rects
+    //     .enter()
+    //     .append("rect")
+    //     .attr("class", "bar")
+    //     .attr("x", function (d) {
+    //       return xscale(d.label);
+    //     })
+    //     .attr("y", function (d) {
+    //       return yscale(d.value);
+    //     })
+    //     .attr("width", xscale.rangeBand())
+    //     .attr("height", function (d) {
+    //       return height - yscale(d.value);
+    //     });
+
+    //   label
+    //     .enter()
+    //     .append("text")
+    //     .attr("class", "label")
+    //     .attr("x", function (d) {
+    //       return xscale(d.label);
+    //     })
+    //     .style("fill", (d) => {
+    //       return d.value ===
+    //         d3.max(data, (d) => {
+    //           return d.value;
+    //         })
+    //         ? "red"
+    //         : "red";
+    //     })
+    //     .attr("y", function (d) {
+    //       return yscale(d.value) - 5;
+    //     })
+    //     .text((d) => {
+    //       return d.value;
+    //     })
+    //     .attr("width", xscale.rangeBand())
+    //     .attr("height", function (d) {
+    //       return height - yscale(d.value);
+    //     });
+
+    //   label.exit().remove();
+    //   rects.exit().remove();
+    // }
+  }
+
+  private dimensions(): number[] {
     const { margin } = this;
     return [
       this.width - margin.left - margin.right,
--- out/app.js
+++ out/app.js
@@ -42,18 +42,18 @@ function fetchData() {
             switch (_a.label) {
                 case 0:
                     _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch("https://api.covid19api.com/live/country/ukraine/status/confirmed")];
+                    return [4 /*yield*/, fetch("https://api.covid19api.com/total/country/south-africa/status/confirmed?from=2020-03-21T00:00:00Z&to=2021-03-01T00:00:00Z")];
                 case 1:
                     response = _a.sent();
                     return [4 /*yield*/, response.json()];
                 case 2:
                     data = _a.sent();
-                    // let slicedData = data.length ? data.slice(0, 1000) : [];
                     new ChartsCreator(".main", { data: data, type: "Bar" }).init();
                     return [2 /*return*/, data];
                 case 3:
                     error_1 = _a.sent();
-                    throw new Error(error_1);
+                    console.log(error_1);
+                    return [3 /*break*/, 4];
                 case 4: return [2 /*return*/];
             }
         });
@@ -63,7 +63,7 @@ fetchData();
 var ChartsCreator = /** @class */ (function () {
     function ChartsCreator(element, options) {
         this.defaults = {
-            width: 900,
+            width: 1300,
             height: 400,
             margin: {
                 top: 15,
@@ -82,18 +82,6 @@ var ChartsCreator = /** @class */ (function () {
         Object.assign(this, this.defaults, options, types);
         this.dataConfigurator();
     }
-    ChartsCreator.prototype.dataConfigurator = function () {
-        var configData = this.data.map(function (el) {
-            var format = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
-            var obj = {
-                date: format(el.Date),
-                value: el.Confirmed,
-            };
-            return obj;
-        });
-        this.data = configData;
-        console.log(this);
-    };
     ChartsCreator.prototype.init = function () {
         var margin = this.margin;
         var svg = (this.svg = d3
@@ -101,14 +89,28 @@ var ChartsCreator = /** @class */ (function () {
             .append("svg")
             .attr("width", this.width)
             .attr("height", this.height)
-            .attr("overflow", "visible")
-            .append("g")
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")"));
+            .attr("overflow", "visible"));
+        this.createBarChart(this.data);
+        // this.createLineChart(this.data);
+    };
+    ChartsCreator.prototype.dataConfigurator = function () {
+        var configData = this.data.map(function (el) {
+            // const format = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
+            var obj = {
+                date: el.Date,
+                value: el.Cases,
+            };
+            return obj;
+        });
+        configData.sort(function (a, b) { return b.value - a.value; });
+        this.data = configData;
+    };
+    ChartsCreator.prototype.createLineChart = function (data) {
         var x = d3
             .scaleTime()
             .domain(d3.extent(this.data, function (d) { return d.date; }))
             .range([0, this.width]);
-        var xAxis = svg
+        var xAxis = this.svg
             .append("g")
             .attr("transform", "translate(0," + this.height + ")")
             .call(d3.axisBottom(x));
@@ -116,12 +118,10 @@ var ChartsCreator = /** @class */ (function () {
             .scaleLinear()
             .domain([0, d3.max(this.data, function (d) { return +d.value; })])
             .range([this.height, 0]);
-        var yAxis = svg.append("g").call(d3.axisLeft(y));
-        this.renderLineChart(x, y);
-    };
-    ChartsCreator.prototype.renderLineChart = function (x, y) {
+        var yAxis = this.svg.append("g").call(d3.axisLeft(y));
         var scaleX = (this.scaleY = d3.scaleTime().range([0, innerWidth]));
         var scaleY = (this.scaleY = d3.scaleLinear().range([innerHeight, 0]));
+        var dateFormatter = d3.timeFormat("%Y/%m/%d");
         this.line = d3
             .line()
             .curve(this.lineCurve)
@@ -157,9 +157,9 @@ var ChartsCreator = /** @class */ (function () {
         var idleTimeout;
         area
             .append("path")
-            .datum(this.data)
+            .datum(data)
             .attr("class", "myArea")
-            .attr("fill", "red")
+            .attr("fill", "lightgrey")
             .attr("fill-opacity", 0.3)
             .attr("stroke", "grey")
             .attr("stroke-width", 1)
@@ -187,16 +187,17 @@ var ChartsCreator = /** @class */ (function () {
             .attr("x", 30)
             .attr("y", 0);
         function mousemove() {
-            var x0 = x.invert(d3.pointer("mousemove", this)[0]), i = bisectDate(this.data, x0, 1), d0 = this.data[i - 1], d1 = this.data[i], d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+            var x0 = x.invert(d3.pointer(event, this)[0]), i = bisectDate(data, x0, 1), d0 = data[i - 1], d1 = data[i], d = x0 - d0.date > d1.date - x0 ? d1 : d0;
             focus.attr("transform", "translate(" + x(d.date) + "," + y(d.value) + ")");
-            focus.select(".tooltip-date").text(d.date);
-            focus.select(".tooltip-likes").text(d.value + "$");
+            focus.select(".tooltip-date").text(dateFormatter(d.date));
+            focus.select(".tooltip-likes").text(d.value);
         }
         function idled() {
             idleTimeout = null;
         }
-        function updateChart() {
-            var extent = d3.selection;
+        function updateChart(_a) {
+            var selection = _a.selection;
+            var extent = selection;
             if (!extent) {
                 if (!idleTimeout)
                     return (idleTimeout = setTimeout(idled, 350));
@@ -206,7 +207,7 @@ var ChartsCreator = /** @class */ (function () {
                 x.domain([x.invert(extent[0]), x.invert(extent[1])]);
                 area.select(".brush").call(brush.move, null);
             }
-            this.xAxis.transition().duration(1000).call(d3.axisBottom(x));
+            xAxis.transition().duration(1000).call(d3.axisBottom(x));
             area
                 .select(".myArea")
                 .transition()
@@ -214,11 +215,11 @@ var ChartsCreator = /** @class */ (function () {
                 .attr("d", areaGenerator);
         }
         this.svg
-            // .on("dblclick", function (): any {
-            //   x.domain(d3.extent(this.data, (d: line) => d.date));
-            //   this.xAxis.transition().call(d3.axisBottom(x));
-            //   area.select(".myArea").transition().attr("d", areaGenerator);
-            // })
+            .on("dblclick", function () {
+            x.domain(d3.extent(data, function (d) { return d.date; }));
+            xAxis.transition().call(d3.axisBottom(x));
+            area.select(".myArea").transition().attr("d", areaGenerator);
+        })
             .on("mouseover", function () {
             focus.style("display", null);
         })
@@ -226,6 +227,190 @@ var ChartsCreator = /** @class */ (function () {
             focus.style("display", "none");
         })
             .on("mousemove", mousemove);
+    };
+    ChartsCreator.prototype.createBarChart = function (data) {
+        var _this = this;
+        var maxLength = d3.max(data.map(function (d) { return d.date.length; }));
+        var marginOverview = { top: 30, right: 10, bottom: 20, left: 40 };
+        var barWidth = maxLength * 5;
+        var numBars = Math.round(this.width / barWidth);
+        var isScrollDisplayed = barWidth * data.length > this.width;
+        var heightOverview = 80 - marginOverview.top - marginOverview.bottom;
+        var selectorHeight = 40;
+        var dateFormatter = d3.timeFormat("%Y/%m/%d");
+        var xscale = d3
+            .scaleBand()
+            .domain(data.slice(0, numBars).map(function (d) {
+            return d.date;
+        }))
+            .range([0, this.width]);
+        var yscale = d3
+            .scaleLinear()
+            .domain([
+            0,
+            d3.max(data.slice(0, numBars), function (d) {
+                return +d.value;
+            }),
+        ])
+            .range([this.height, 0]);
+        var diagram = this.svg
+            .append("g")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+        diagram
+            .append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, " + this.height + ")")
+            .call(d3.axisBottom(xscale));
+        diagram.append("g").attr("class", "y axis").call(d3.axisLeft(yscale));
+        var bars = diagram.append("g");
+        bars
+            .selectAll("rect")
+            .data(data.slice(0, numBars), function (d) {
+            return d.date;
+        })
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("y", this.height)
+            .attr("height", 0)
+            .transition()
+            .duration(750)
+            .delay(function (d, i) {
+            return i * 150;
+        })
+            .attr("x", function (d) {
+            return xscale(d.date);
+        })
+            .attr("y", function (d) { return yscale(d.value); })
+            .attr("height", function (d) { return _this.height - yscale(d.value); })
+            .attr("width", xscale.bandwidth() - 3);
+        // if (isScrollDisplayed) {
+        //   let xOverview = d3
+        //     .scaleBand()
+        //     .domain(
+        //       data.map(function (d) {
+        //         return d.label;
+        //       })
+        //     )
+        //     .range([0, this.width]);
+        //   let yOverview = d3.scaleLinear().range([heightOverview, 0]);
+        //   yOverview.domain(yscale.domain());
+        //   let subBars = diagram.selectAll(".subBar").data(data);
+        //   subBars
+        //     .enter()
+        //     .append("rect")
+        //     .classed("subBar", true)
+        //     .attr({
+        //       height: function (d) {
+        //         return heightOverview - yOverview(d.value);
+        //       },
+        //       width: function (d) {
+        //         return xOverview.rangeBand();
+        //       },
+        //       x: function (d) {
+        //         return xOverview(d.label);
+        //       },
+        //       y: function (d) {
+        //         return height + heightOverview + yOverview(d.value);
+        //       },
+        //     });
+        //   var displayed = d3
+        //     .scaleQuantize()
+        //     .domain([0, this.width])
+        //     .range(d3.range(data.length));
+        //   diagram
+        //     .append("rect")
+        //     .attr(
+        //       "transform",
+        //       "translate(0, " + (this.height + this.margin.bottom) + ")"
+        //     )
+        //     .attr("class", "mover")
+        //     .attr("x", 0)
+        //     .attr("y", 0)
+        //     .attr("height", selectorHeight)
+        //     .attr(
+        //       "width",
+        //       Math.round(parseFloat(numBars * this.width) / data.length)
+        //     )
+        //     .attr("pointer-events", "all")
+        //     .attr("cursor", "ew-resize")
+        //     .call(d3.drag().on("drag", display));
+        //   this.svg.on("scroll", display);
+        // }
+        // function display() {
+        //   let x = parseInt(d3.select(this).attr("x")),
+        //     nx = x + d3.event.dx,
+        //     w = parseInt(d3.select(this).attr("width")),
+        //     f,
+        //     nf,
+        //     new_data,
+        //     rects;
+        //   if (nx < 0 || nx + w > width) return;
+        //   d3.select(this).attr("x", nx);
+        //   f = displayed(x);
+        //   nf = displayed(nx);
+        //   if (f === nf) return;
+        //   new_data = data.slice(nf, nf + numBars);
+        //   xscale.domain(
+        //     new_data.map(function (d) {
+        //       return d.label;
+        //     })
+        //   );
+        //   diagram.select(".x.axis").call(xAxis);
+        //   rects = bars.selectAll("rect").data(new_data, function (d) {
+        //     return d.label;
+        //   });
+        //   label = labels.selectAll(".label").data(new_data, function (d) {
+        //     return d.label;
+        //   });
+        //   rects.attr("x", function (d) {
+        //     return xscale(d.label);
+        //   });
+        //   label.attr("x", function (d) {
+        //     return xscale(d.label);
+        //   });
+        //   rects
+        //     .enter()
+        //     .append("rect")
+        //     .attr("class", "bar")
+        //     .attr("x", function (d) {
+        //       return xscale(d.label);
+        //     })
+        //     .attr("y", function (d) {
+        //       return yscale(d.value);
+        //     })
+        //     .attr("width", xscale.rangeBand())
+        //     .attr("height", function (d) {
+        //       return height - yscale(d.value);
+        //     });
+        //   label
+        //     .enter()
+        //     .append("text")
+        //     .attr("class", "label")
+        //     .attr("x", function (d) {
+        //       return xscale(d.label);
+        //     })
+        //     .style("fill", (d) => {
+        //       return d.value ===
+        //         d3.max(data, (d) => {
+        //           return d.value;
+        //         })
+        //         ? "red"
+        //         : "red";
+        //     })
+        //     .attr("y", function (d) {
+        //       return yscale(d.value) - 5;
+        //     })
+        //     .text((d) => {
+        //       return d.value;
+        //     })
+        //     .attr("width", xscale.rangeBand())
+        //     .attr("height", function (d) {
+        //       return height - yscale(d.value);
+        //     });
+        //   label.exit().remove();
+        //   rects.exit().remove();
+        // }
     };
     ChartsCreator.prototype.dimensions = function () {
         var margin = this.margin;
@@ -239,3 +424,4 @@ var ChartsCreator = /** @class */ (function () {
     };
     return ChartsCreator;
 }());
+//# sourceMappingURL=app.js.map
